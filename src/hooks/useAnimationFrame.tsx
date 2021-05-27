62774a7ad0a9f6c7e90b40bee2c9ab6ab6e27579
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-05-26 22:50:30 -0300

    ✨ create useAnimationFrame hook

---
+++ src/hooks/useAnimationFrame.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+function useAnimationFrame(callback: FrameRequestCallback) {
+  const callbackRef = useRef(callback);
+
+  if (callbackRef.current !== callback) {
+    callbackRef.current = callback;
+  }
+  
+  useEffect(() => {
+    let handle: number;
+
+    const run = (time: number) => {
+      handle = window.requestAnimationFrame(run);
+      callbackRef.current(time);
+    };
+
+    run(window.performance.now());
+
+    return () => {
+      if (!handle) return;
+
+      window.cancelAnimationFrame(handle);
+    };
+  }, []);
+}
+
+export default useAnimationFrame;
