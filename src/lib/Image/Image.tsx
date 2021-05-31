7df5dcadbb472fd68fae108a85b19b02dc7b4e24
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-05-30 23:47:56 -0300

    ✨ add translate and rotate props to Image

--- src/lib/Image/Image.tsx
+++ src/lib/Image/Image.tsx
@@ -6,17 +6,31 @@ export type Position = [x: number, y: number];
 type Props = {
   width?: number;
   height?: number;
+  rotate?: number;
   source: HTMLImageElement;
   position?: Position;
+  translate?: Position;
 };
 
-function Image({ source, position: [x, y] = [0, 0], width, height }: Props): null {
+function Image(props: Props): null {
   const context = useCanvasContext();
+  const {
+    width,
+    height,
+    rotate = 0,
+    source,
+    position: [x, y] = [0, 0],
+    translate: [translateX, translateY] = [0, 0],
+  } = props;
 
   useLayoutEffect(() => {
+    context.save();
+
+    context.rotate(rotate);
+    context.translate(translateX, translateY);
     context.drawImage(source, x, y, width ?? source.width, height ?? source.height);
 
-    return () => context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.restore();
   });
 
   return null;
