2e21d651ff4b7ab1bedec102d6eb201b7dcd044a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-10-22 00:25:44 -0300

    🎨 reexports CycleContextValue

--- src/lib/cycle/index.ts
+++ src/lib/cycle/index.ts
@@ -1,3 +1,4 @@
 export { default as Cycle } from './Cycle';
 export { default as useCycle } from './useCycle';
+export type { CycleContextValue } from './CycleContext';
 export type { default as CycleOperation } from './CycleOperation';
