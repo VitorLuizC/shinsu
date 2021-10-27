fa3c9f825d748e61996ea020b422a9b1ae058df1
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-10-27 02:16:07 -0300

    ✨ create Rectangle component

---
+++ src/lib/shape/Rectangle.tsx
@@ -0,0 +1,52 @@
+import { useRenderInCycle } from 'lib/render';
+import type Color from './Color';
+
+type Props = {
+  width: number;
+  height: number;
+  fillColor?: Color;
+  positionX?: number;
+  positionY?: number;
+  strokeWidth?: number;
+  strokeColor?: Color;
+};
+
+function Rectangle(props: Props): null {
+  const {
+    width,
+    height,
+    fillColor,
+    strokeColor,
+    strokeWidth,
+    positionX = 0,
+    positionY = 0,
+  } = props;
+
+  useRenderInCycle((context) => {
+    context.save();
+    context.beginPath();
+
+    if (fillColor !== undefined)
+      context.fillStyle = fillColor;
+
+    if (strokeColor !== undefined)
+      context.strokeStyle = strokeColor;
+
+    if (strokeWidth !== undefined)
+      context.lineWidth = strokeWidth;
+
+    context.rect(positionX, positionY, width, height);
+
+    if (fillColor !== undefined)
+      context.fill();
+
+    if (strokeColor !== undefined || strokeWidth !== undefined)
+      context.stroke();
+    context.closePath();
+    context.restore();
+  });
+
+  return null;
+}
+
+export default Rectangle;
--- src/lib/shape/index.tsx
+++ src/lib/shape/index.tsx
@@ -1,2 +1,3 @@
 export { default as Line } from './Line';
 export { default as Circle } from './Circle';
+export { default as Rectangle } from './Rectangle';
